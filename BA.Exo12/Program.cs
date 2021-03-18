bfa741925d714be8e0a736b2cb535f5dd5aa1f49
Legrain Samuel <samuel.legrain@bstorm.be>
2021-03-18 08:46:35 +0100

    20210317 - Ajout correctifs 12 et 13

---
+++ BA.Exo12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BA.Exo12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Veuillez indiquer le nombre à modifier :");
+            int number = int.Parse(Console.ReadLine());
+            int nbDecimal = 0;
+            double result = 0, rest = number;
+            while(nbDecimal < 11 && rest != 0)
+            {
+                int value = 0;
+                while (number >= (Math.Pow(result + (value / Math.Pow(10, nbDecimal)),2)))
+                {
+                    value++;
+                }
+                rest = (rest*10) % (value-1);
+                result = result + ((value-1) / Math.Pow(10, nbDecimal));
+                nbDecimal++;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ BA.Exo13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace BA.Exo13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Veuillez introduire un nombre que les nombres ne peuvent dépasser :");
+            int maxValue = int.Parse(Console.ReadLine());
+            Queue queue = new Queue();
+            int value = 2;
+            while (value < maxValue)
+            {
+                bool isNbPremier = true;
+                int diviseur = 2;
+                while (diviseur < (value/2)+1 && isNbPremier)
+                {
+                    if (value % diviseur == 0) isNbPremier = false;
+                    diviseur++;
+                }
+                if (isNbPremier)
+                {
+                    queue.Enqueue(value);
+                }
+                value++;
+            }
+            foreach (int nbPremier in queue)
+            {
+                Console.WriteLine(nbPremier);
+            }
+
+        }
+    }
+}
